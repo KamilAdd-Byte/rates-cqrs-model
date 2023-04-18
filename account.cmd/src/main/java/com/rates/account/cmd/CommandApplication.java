c28ca20fca2560a8d549971c97e34a66ed18503d
Kamil Sulejewski <kamillodzinski09@gmail.com>
2023-04-18 22:30:19 +0200

    CMD: chore: Removed unnecessary lines.

--- account.cmd/src/main/java/com/rates/account/cmd/CommandApplication.java
+++ account.cmd/src/main/java/com/rates/account/cmd/CommandApplication.java
@@ -14,14 +14,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import javax.annotation.PostConstruct;
 
-
-//@ComponentScan(basePackages = {"com.rates.core.*","com.rates.core.infrastructures"})
 @ComponentScan(basePackageClasses= {
 		EventSourcingHandler.class,
 		CurrencyRequestEventStore.class,
@@ -40,7 +37,6 @@ public class CommandApplication {
 
 	@Autowired
 	private CommandHandler commandHandler;
-
 
 	public static void main(String[] args) {
 		 BeanContextDisplayer.of(SpringApplication.run(CommandApplication.class, args));
