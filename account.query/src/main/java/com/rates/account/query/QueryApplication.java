0733ab0a1882cb4fe53193785cbd6314c4f9f2ed
Kamil Sulejewski <kamillodzinski09@gmail.com>
2023-05-23 11:20:14 +0200

    QRY [#24] feat: Registered handle method for QueryApplication.

--- account.query/src/main/java/com/rates/account/query/QueryApplication.java
+++ account.query/src/main/java/com/rates/account/query/QueryApplication.java
@@ -1,13 +1,33 @@
 package com.rates.account.query;
 
+import com.rates.account.query.api.queries.FindAllCurrencyRequests;
+import com.rates.account.query.api.queries.FindCurrencyRequestByIdQuery;
+import com.rates.account.query.api.queries.QueryHandler;
+import com.rates.core.infrastructures.QueryDispatcher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class QueryApplication {
 
+	@Autowired
+	private QueryDispatcher queryDispatcher;
+
+	@Autowired
+	private QueryHandler queryHandler;
+
 	public static void main(String[] args) {
 		SpringApplication.run(QueryApplication.class, args);
+	}
+
+
+	@PostConstruct
+	public void registerHandlers() {
+		queryDispatcher.registerHandler(FindAllCurrencyRequests.class, queryHandler::handle);
+		queryDispatcher.registerHandler(FindCurrencyRequestByIdQuery.class, queryHandler::handle);
 	}
 
 }
